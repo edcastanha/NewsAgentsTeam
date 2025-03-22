3034a81ec7f02aa7c0be12ed7a773d2724d01995
Edson Bezerra <edcastanha@gmail.com>
2025-03-22 19:12:53 -0300

    Reset do projeto, localstack nao suportado.

--- django_api/news/serializer.py
+++
@@ -1,8 +0,0 @@
-# Serializer Django REST do app news
-from rest_framework import serializers
-from .models import NewsModel
-
-class NewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = NewsModel
-        fields = '__all__'
